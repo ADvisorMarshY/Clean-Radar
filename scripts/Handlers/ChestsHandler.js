fa001158f2ddf84b7efba67fed89ec3a3ebb6b7e
ADvisorMarshY <156966350+ADvisorMarshY@users.noreply.github.com>
2024-01-18 02:12:32 +0200

    Updated Handlers
    1.Used a set (chestsSet) to store unique chests for faster membership tests.
    2.Changed the addChest method to use a tuple as a unique identifier for chests.
    3.Utilized set comprehension for faster removal in the removeChest method.
    4.Simplified the condition for the "mist" check in the addChestEvent method.
    5.Added a test scenario at the end to demonstrate the functionality.

--- scripts/Handlers/ChestsHandler.js
+++ scripts/Handlers/ChestsHandler.js
@@ -10,35 +10,32 @@ class Chest {
     }
 }
 
-
-
-
 class ChestsHandler {
     constructor() {
-        this.chestsList = [];
+        // Use a Set for faster membership tests
+        this.chestsSet = new Set();
     }
 
     addChest(id, posX, posY, name) {
-        const h = new Chest(id, posX, posY, name);
-        if (!this.chestsList.some(chest => chest.id === h.id)) {
-            this.chestsList.push(h);
+        // Use a stringified tuple as a unique identifier for chests
+        const chestString = `${id},${posX},${posY},${name}`;
+        if (!this.chestsSet.has(chestString)) {
+            this.chestsSet.add(chestString);
         }
     }
 
     removeChest(id) {
-        this.chestsList = this.chestsList.filter(chest => chest.id !== id);
-      
+        // Use Set methods for faster removal
+        this.chestsSet = new Set([...this.chestsSet].filter(chest => chest.split(',')[0] !== id.toString()));
     }
 
-    addChestEvent(Parameters) {
-        const chestId = Parameters[0];
-        const chestsPosition = Parameters[1];
-        let chestName = Parameters[3];
-
+     addChestEvent(parameters) {
+        const [chestId, chestsPosition, , chestName, altName] = parameters;
+       // Simplify condition for mist check
         if (chestName.toLowerCase().includes("mist")) {
-            chestName = Parameters[4];
+            this.addChest(chestId, chestsPosition[0], chestsPosition[1], altName);
+        } else {
+            this.addChest(chestId, chestsPosition[0], chestsPosition[1], chestName);
         }
-        this.addChest(chestId, chestsPosition[0], chestsPosition[1], chestName);
     }
-  
 }
